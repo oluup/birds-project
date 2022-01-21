93b334aa797a66c9f009d54c441f4cf64a25a99f
Yasar ICLI <yasaricli@gmail.com>
2022-01-21 23:03:50 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
 const path = require("path");
 
 // node env variables.
-const { COLLECTION_ADDRESS } = process.env;
+const { COLLECTION_ADDRESS, MINT_PRICE } = process.env;
 
 let serverRuntimeConfig = {
   COLLECTION_ADDRESS,
+  MINT_PRICE,
 };
 
 let publicRuntimeConfig = {
   COLLECTION_ADDRESS,
+  MINT_PRICE,
 };
 
 module.exports = {
   serverRuntimeConfig,
   publicRuntimeConfig,
-  
+
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
